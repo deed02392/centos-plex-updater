546d669ddf7483f1e4920a77da379324e0136ac6
deed02392 <georgehafiz@gmail.com>
2017-08-17 09:47:15 +0100

    Start the service after updating, because currently the package updater does not.

--- get-install-plex-update.sh
+++ get-install-plex-update.sh
@@ -3,4 +3,7 @@ yum update || exit 1
 echo "Enter/paste RPM download URI: "
 read download_uri &&
 wget "$download_uri" -O plexmediaserver.rpm &&
-yum install ./plexmediaserver.rpm
+yum install ./plexmediaserver.rpm &&
+systemctl start plexmediaserver &&
+sleep 3 &&
+systemctl status plexmediaserver
