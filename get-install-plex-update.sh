f3107988f43cbc14d792af41731185f897f38311
deed02392 <georgehafiz@gmail.com>
2017-12-13 21:50:32 +0000

    Use a secure tmpdir location to enable script execution from anywhere (you can now add the script and use it from /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin)

--- get-install-plex-update.sh
+++ get-install-plex-update.sh
@@ -2,8 +2,10 @@
 yum update || exit 1
 echo "Enter/paste RPM download URI: "
 read download_uri &&
-wget "$download_uri" -O plexmediaserver.rpm &&
-yum install ./plexmediaserver.rpm &&
+TMPDIR=$(mktemp -td plexmediaserver.XXXXXXXXXX) || { echo 'mktemp failed.'; exit 1; }
+trap 'rm -rf "$TMPDIR"' EXIT
+cd "$TMPDIR" && { wget "$download_uri" -O plexmediaserver.rpm; cd -; } &&
+yum install "$TMPDIR/plexmediaserver.rpm" &&
 systemctl start plexmediaserver &&
 sleep 3 &&
 systemctl status plexmediaserver
